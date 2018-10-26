9be8c0a57a02a647d8197514b8440a4f27264b62
Muhammad Azhdari <ma313@aut.ac.ir>
2018-10-27 03:05:02 +0330

    change question1 but it has had bug yet

--- HW1/q1.py
+++ HW1/q1.py
@@ -1,13 +1,28 @@
 n = int(input())
 
-commands = list()
-stack = list()
+commands = []
+stack = []
+min = int()
 
-for i in range(0, n):
-    command = input()
-    if command.split(" ")[0] == "push":
-        stack.append(int(command.split(" ")[1]))
-    elif command.split(" ")[0] == "pop":
-        stack.pop()
+for i in range(n):
+    commands.append(input())
+
+for command in commands:
+    if command.__contains__("push"):
+        x = int(command.split()[1])
+        if len(stack) == 0:
+            min = x
+            stack.append(x)
+        if x < min:
+            stack.append(2*x - min)
+            min = x
+        else:
+            stack.append(x)
+    elif command.__contains__("pop"):
+        if len(stack) == 0:
+            continue
+        top = stack.pop()
+        if top < min:
+            min = 2 * min - top
     else:
-        print(min(stack))
+        print(min)
