bb3cfc47fe7317ebd800aab1c92c49ac4a7fd4b4
Muhammad Azhdari <ma313@aut.ac.ir>
2018-10-27 13:42:19 +0330

    start q2

--- HW1/q2.py
+++ HW1/q2.py
@@ -1,15 +1,37 @@
+from collections import deque
+from queue import Queue
+
+
 x = int(input())
 
 commands = list()
-queue = list()
-poped_list = list()
+queue = Queue()
+undo_list = list()
 for i in range(0, x):
     commands.append(input())
 
 for command in commands:
     if command.split(" ")[0] == "enqueue":
-        queue.insert(0, int(command.split(" ")[1]))
-    elif command.split(" ")[0] == "pop":
-       poped_list.append(queue.pop())
+        x = int(command.split(" ")[1])
+        queue.put_nowait(x)
+        undo_list.append("pop".format(x))
 
-print(poped_list)
+    elif command.split(" ")[0] == "pop":
+        # print(queue[0])
+        pop_element = queue.get()
+        print(pop_element)
+        undo_list.append("enqueue {}".format(pop_element))
+
+
+
+    else:
+        if undo_list[-1].__contains__("enqueue"):
+            # print(undo_list[-1])
+            queue.put(int(undo_list[-1].split(" ")[1]))
+            # print("q0" + str(queue[0]))
+            undo_list.remove(undo_list[-1])
+            continue
+        else:
+            # print(undo_list[-1])
+            queue.get()
+            undo_list.remove(undo_list[-1])
