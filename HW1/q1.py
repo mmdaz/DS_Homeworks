ad4fba336c04b71d48c5896399ddc491079f25c3
Muhammad Azhdari <ma313@aut.ac.ir>
2018-10-27 12:52:28 +0330

    _muhmdreza_ debug my code and q1 compeleted :)

--- HW1/q1.py
+++ HW1/q1.py
@@ -4,23 +4,22 @@ commands = []
 stack = []
 min = int()
 
-for i in range(n):
-    commands.append(input())
+# for i in range(n):
+#     commands.append(input())
 
-for command in commands:
+for i in range(n):
+    command = input()
     if command.__contains__("push"):
         x = int(command.split()[1])
         if len(stack) == 0:
             min = x
             stack.append(x)
-        if x < min:
+        elif x < min:
             stack.append(2*x - min)
             min = x
         else:
             stack.append(x)
     elif command.__contains__("pop"):
-        if len(stack) == 0:
-            continue
         top = stack.pop()
         if top < min:
             min = 2 * min - top
