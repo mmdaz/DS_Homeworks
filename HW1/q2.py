0ff1f523a43f095ec34f651127b7f383a8605573
Muhammad Azhdari <ma313@aut.ac.ir>
2018-10-27 16:00:00 +0330

    q2 completed

--- HW1/q2.py
+++ HW1/q2.py
@@ -1,11 +1,10 @@
 from collections import deque
 from queue import Queue
 
-
 x = int(input())
 
 commands = list()
-queue = Queue()
+queue = deque()
 undo_list = list()
 for i in range(0, x):
     commands.append(input())
@@ -13,25 +12,20 @@ for i in range(0, x):
 for command in commands:
     if command.split(" ")[0] == "enqueue":
         x = int(command.split(" ")[1])
-        queue.put_nowait(x)
+        queue.appendleft(x)
         undo_list.append("pop".format(x))
 
     elif command.split(" ")[0] == "pop":
-        # print(queue[0])
-        pop_element = queue.get()
+        pop_element = queue.pop()
         print(pop_element)
         undo_list.append("enqueue {}".format(pop_element))
-
-
 
     else:
         if undo_list[-1].__contains__("enqueue"):
             # print(undo_list[-1])
-            queue.put(int(undo_list[-1].split(" ")[1]))
-            # print("q0" + str(queue[0]))
-            undo_list.remove(undo_list[-1])
-            continue
+            # print("qs{}".format(queue.qsize()))
+            queue.append(int(undo_list[-1].split(" ")[1]))
+            undo_list.pop()
         else:
-            # print(undo_list[-1])
-            queue.get()
-            undo_list.remove(undo_list[-1])
+            queue.popleft()
+            undo_list.pop()
