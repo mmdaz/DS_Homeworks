91a3ee780fb6eae6dc6c6a82e53c8601700beaba
Muhammad Azhdari <ma313@aut.ac.ir>
2018-11-05 04:37:12 +0330

    q4 comleted . #remove takes time : dont use it in competitive questions

--- HW1/q4.py
+++ HW1/q4.py
@@ -9,13 +9,25 @@ weighs.sort()
 weighs_sum = 0
 branches = 0
 
-print(weighs)
+# print(weighs)
 
-for w in weighs:
-    weighs_sum += w
-    if weighs_sum >= 10:
-        if weighs_sum == x:
-            branches += 1
+end = n - 1
+begin = 0
+while end != begin and begin != end + 1:
+
+    if weighs[begin] + weighs[end] <= x:
+        branches += 1
+        begin += 1
+        end -= 1
+
+    else:
+        branches += 1
+        end -= 1
+
+if end == begin:
+    branches += 1
+
+
 
 
 print(branches)
