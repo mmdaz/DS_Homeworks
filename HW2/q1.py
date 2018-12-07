d3c42d1dbf5cfa28c084730ec8110aa12c395f02
Muhammad Azhdari <ma313@aut.ac.ir>
2018-12-07 19:05:33 +0330

    q1 completed

--- HW2/q1.py
+++ HW2/q1.py
@@ -1,93 +1,132 @@
-import heapq
-
 # left_count = 0
 
 
-class Binary_Search_Tree:
+class Node:
     def __init__(self, data):
+        self.left_count = 0
         self.data = data
         self.right = None
         self.left = None
-        self.left_count = 0
 
-    def insert_tree(self, x):
-        # print("l : {}".format(self.left_count))
 
-        if self.data:
-            if x <= self.data:
-                if self.left is None:
-                    # global left_count
-                    self.left_count += 1
-                    self.left = Binary_Search_Tree(data=x)
-                else:
-                    self.left.insert_tree(x=x)
-            elif x > self.data:
-                if self.right is None:
-                    self.right = Binary_Search_Tree(data=x)
-                else:
-                    self.right.insert_tree(x=x)
+class BinarySearchTree:
+    def __init__(self, root):
+        self.root = root
+
+    def insert_in_tree(self, x):
+        pTraverse = self.root
+        current_node = self.root
+
+        if self.root is None:
+            self.root = x
         else:
-            self.data = x
+            while pTraverse:
+                current_node = pTraverse
+                if x.data < pTraverse.data:
+                    pTraverse.left_count += 1
+                    pTraverse = pTraverse.left
+                else:
+                    pTraverse = pTraverse.right
+            if x.data < current_node.data:
+                current_node.left = x
+            else:
+                current_node.right = x
 
-    def k_th_smallest_data(self, k):
+    def kth_smallest_number(self, k):
+        result = -1
+        pTraverce = self.root
+        while pTraverce:
+            if pTraverce.left_count + 1 == k:
+                result = pTraverce.data
+                break
+            elif pTraverce.left_count < k:
+                k = k - (pTraverce.left_count + 1)
+                pTraverce = pTraverce.right
+            else:
+                pTraverce = pTraverce.left
 
-        # print(self.left_count)
-        if self.left_count + 1 == k:
+        return result
+
+
+def insert(root, node):
+    if root[0] is None:
+        print("a im hrerrrrrrr")
+        root[0] = node
+    else:
+        if root[0].data < node.data:
+            if root[0].right is None:
+
+                root[0].right = node
+            else:
+                root[0].left_count += 1
+                insert(root, node)
+        else:
+            if root[0].left is None:
+                root[0].left = node
+            else:
+                insert(root, node)
+
+
+def kthSmallest(arr, k):
+    # Sort the given array
+    arr.sort()
+
+    # Return k'th element in the
+    # sorted array
+    return arr[k - 1]
+
+
+def k_th_smallest(root, k):
+    pTraverse = root
+    while pTraverse:
+        print(pTraverse.left_count)
+        if pTraverse.left_count + 1 == k:
             print("salam")
-            print(self.data)
-            return self.data
-        if k > self.left_count:
-            k = k - (self.left_count + 1)
-            self.right.k_th_smallest_data(k)
+            return pTraverse.data
+        elif k > pTraverse.left_count:
+            k = k - (pTraverse.left_count + 1)
+            pTraverse = pTraverse.right
         else:
-            self.left.k_th_smallest_data(k)
-
-        # pTraverse = self.data
-        # # global left_count
-        # print(str(k) + "k")
-        # # print(str(left_count) + "dfg")
-        # while pTraverse:
-        #     print(str(self.data) + "sdf")
-        #     if pTraverse.left_count + 1 == k:
-        #         print("salam")
-        #         return self.data
-        #     elif k > pTraverse.left_count:
-        #         k = k - (pTraverse.left_count + 1)
-        #         pTraverse = pTraverse.right
-        #     else:
-        #         pTraverse = pTraverse.left
-
-    def print_tree(self):
-        if self.left:
-            self.left.print_tree()
-        print(self.data),
-        if self.right:
-            self.right.print_tree()
+            pTraverse = pTraverse.left
 
 
 n = int(input())
-
 numbers = list()
 row_inputs = list()
 
-for i in range(10):
-    row_inputs.append(input())
-
-bst = Binary_Search_Tree(data=None)
+# for i in range(10):
+#     row_inputs.append(input())
+root = [None]
 counter = 0
-for row_input in row_inputs:
-    command = row_input.split(" ")[0]
-    if int(command) == 1:
-        vote = row_input.split(" ")[1]
-        counter += 1
-        bst.insert_tree(int(vote))
-
-    else:
+bst = BinarySearchTree(None)
+inps = []
+for i in range(n):
+    # raw_input = input()
+    inps.append(input())
+    if len(inps[i]) == 1:
         if counter < 3:
             print("No reviews yet")
         else:
-            print("sdfg{}".format(bst.k_th_smallest_data(int(counter / 3))))
+            print(bst.kth_smallest_number((counter - int(counter / 3) + 1)))
 
-bst.print_tree()
-print(counter)
-# print(bst.left_count)
+    else:
+        vote = int(inps[i].split(" ")[1])
+        bst.insert_in_tree(Node(vote))
+        counter += 1
+
+#
+# for row_input in row_inputs:
+#     command = row_input.split(" ")[0]
+#     if int(command) == 1:
+#         vote = row_input.split(" ")[1]
+#         counter += 1
+#         numbers.append(int(vote))
+#         bst.insert_in_tree(Node(data=int(vote)))
+#         # insert(root, Node(int(vote)))
+#
+#     else:
+#         if counter < 3:
+#             print("No reviews yet")
+#         else:
+#             print(bst.kth_smallest_number((counter -  int(counter /3) +1)))
+
--- HW2/q2_with_heap.py
+++ HW2/q2_with_heap.py
@@ -1,54 +1,65 @@
-
-def heapify(arr, n, i):
-    largest = i
-    left = 2 * i + 1
-    right = 2 * i + 2
-
-    if left < n and arr[left] > arr[i]:
-        largest = left
-
-    if right < n and arr[right] > arr[i]:
-        largest = right
-
-    if largest != i:
-        arr[i], arr[largest] = arr[largest], arr[i]
-        heapify(arr, n, largest)
+class Node:
+    def __init__(self, value):
+        self.value = value
+        self.count = 0
+        self.left = None
+        self.right = None
 
 
-# noinspection PyShadowingNames
-def heapSort(arr):
-    n = len(arr)
-    for i in range(n, -1, -1):
-        heapify(arr, n, i)
+class BST:
+    def __init__(self):
+        self.root = None
 
-        # One by one extract elements
-    for i in range(n - 1, 0, -1):
-        arr[i], arr[0] = arr[0], arr[i]  # swap
-        heapify(arr, i, 0)
+    def insert(self, value):
+        if self.root is None:
+            self.root = Node(value)
+        else:
+            self.insert_node(Node(value))
+
+    def insert_node(self, new_node):
+        traversal_node = self.root
+        current_node = self.root
+        while traversal_node:
+            current_node = traversal_node
+            if new_node.value < traversal_node.value:
+                traversal_node.count += 1
+                traversal_node = traversal_node.left
+            else:
+                traversal_node = traversal_node.right
+        if new_node.value < current_node.value:
+            current_node.left = new_node
+        else:
+            current_node.right = new_node
+
+    def Kth_smallest(self, k):
+        ret = -1
+        traverse_node = self.root
+        while traverse_node is not None:
+            if traverse_node.count + 1 == k:
+                ret = traverse_node.value
+                break
+            elif traverse_node.count < k:
+                k = k - (traverse_node.count + 1)
+                traverse_node = traverse_node.right
+            else:
+                traverse_node = traverse_node.left
+        return ret
 
 
 n = int(input())
+command = []
 
-numbers = list()
-row_inputs = list()
+numbers = 0
+bst = BST()
 
-for i in range(10):
-    row_inputs.append(input())
-
-counter = 0
-for row_input in row_inputs:
-    command = row_input.split(" ")[0]
-    if int(command) == 1:
-        vote = row_input.split(" ")[1]
-        counter += 1
-        numbers.append(int(vote))
-
-    else:
-        if counter < 3:
+for i in range(n):
+    command.append(input())
+    if command[i].__len__() == 1:
+        if numbers < 3:
             print("No reviews yet")
         else:
-            numbers.sort(reverse=True)
-            # print(int(counter/3 - 1))
-            # print(numbers)
-            print(numbers[int(counter/3) - 1])
+            print(bst.Kth_smallest(numbers - int(numbers / 3) + 1))
 
+    else:
+        bst.insert(int(command[i].split(" ")[1]))
+        numbers += 1
